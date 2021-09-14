96855b0da94dd9edfb108049da1d5de58eafadb1
hemr3 <75843114+hemr3@users.noreply.github.com>
2021-09-14 15:36:43 +0100

    Update abc.R

--- abc.R
+++ abc.R
@@ -50,27 +50,14 @@ abc_sim8 = ABC_rejection(
 #  use_seed = T
 # verbose = T
 )
-                          #above performs abc sim-but remember that tol needs to be somewhat high, and nb above 1
-
-par(mfrow=c(2,2))
-hist(abc_sim$param[,1], main = 'AncSize')
-hist(abc_sim$param[,2], main = 'MigRate P1')
-hist(abc_sim$param[,3], main = 'MigRate P2')
-  #this shows (i think) which params were selected for the sims
-  #acc - i think these are the accepted sims
-
-params = subset(abc_sim6$param)
-param1 = params[,1] #Anc pop size
-param2 = params[,2] #MigRate P1
-param3 = params[,3] #MigRate P2
-  #this splits the abc_sim$param into the three diff ones sim-ed
+  #above performs abc sim-but remember that tol needs to be somewhat high, and nb above 1
 
 rej = abc(target = sum_stat_obs, 
           abc_sim8$param, 
           abc_sim8$stats, 
-          tol = 0.005, #could do rej right now
+          tol = 0.005, #proportion accepted
           method = "rejection")
-  #methods that can be used: rejection, loclinear and neuralnet (produces plots)
+  #methods that can be used: rejection, loclinear and neuralnet (latter two produces plots)
   #rejection method produces which are best 
   #this is estimate of the posterior
 
@@ -120,7 +107,6 @@ vals4 = as.data.frame(vals4)
 vals5 = as.data.frame(vals5)
 vals6 = as.data.frame(vals6)
 vals7 = as.data.frame(vals7)
-
   #for ggplot, must be coerced to df
 
 par(mfrow=c(2,1))
@@ -152,6 +138,7 @@ p2
 #  ggtitle('Selected Simulations')+
 #  theme_minimal()
 #p3
+
   p4 = 
   ggplot(vals2, aes(y=vals1))+
   geom_boxplot(notch = T, outlier.colour = "black", color = "purple")+
@@ -159,6 +146,7 @@ p2
 #  ggtitle('Selected Simulations')+
   theme_minimal()
 
+#boxplot of selected sims below
 p5 =
   ggplot(vals3, aes(y=vals2))+
   geom_boxplot(notch = T, outlier.colour = "black", color = "bisque")+
@@ -190,7 +178,7 @@ p9 =
 #  ggtitle('Selected Simulations')+
   theme_minimal()
 p3+p4+p5+p6+p7+p8+p9
-#boxplot of the selected sims
+
 
 #histograms of selected simulations ####
 p10 = ggplot(vals1, aes(x=vals1))+
@@ -199,8 +187,6 @@ p10 = ggplot(vals1, aes(x=vals1))+
   ylab("Frequency")+
   ggtitle("a: Carrying Capacity Change Factor")+
   theme_minimal()
-
-ggsave("~/Pictures/Visualisations/splatche/350k_selected_distributions005_carcap.png", p10, width = 10, height = 5)
 
 p11 = ggplot(vals2, aes(x=vals2))+
   geom_histogram(fill = c1, alpha = 0.7, bins = 10)+
@@ -255,140 +241,18 @@ p11 + p12 + p13 + p14 + p15 + p16
 ggsave("~/Pictures/Visualisations/splatche/100k_selected_distributions005.png", p10 + p11 + p12 + 
          p13 + p14 + p15 + p16, width = 15, height = 10)
 
-          #analysing the selected sims ####
-sel = read_csv("~/Documents/appendix_samplelist_hetvintro.csv")
+#analysing the selected sims ####
+sel = read_csv("selected_sims.csv")
 
 formattable(sel, align = c("l", "l", "l", "l", "l", "l", "l"))
     #making a little table for the results to include
 
-model = lm(sel$`Ancestral population size` ~ sel$`Neanderthal migration rate` + 
-             sel$`Denisovan migration rate` + 
-             sel$`Neanderthal-Denisovan admixture rate` + 
-             sel$`Denisovan-Neanderthal admixture rate` + 
-             sel$`Neanderthal population growth rate` + 
-             sel$`Denisovan population growth rate`, 
-           data = sel)
-  #multiple linear regression, r = 0.582. not a great model. nonlinear?
-  #only variable that seems signi connected is the anc pop size and mig rate
-  #likely more to do with carcap of deme rather than acc relationship (simmed rel)
-
-model1 = nls(sel$`Ancestral population size` ~ sel$`Neanderthal migration rate`*a + 
-               sel$`Denisovan migration rate`*b + 
-               sel$`Neanderthal-Denisovan admixture rate`*c + 
-               sel$`Denisovan-Neanderthal admixture rate`*d + 
-               sel$`Neanderthal population growth rate`*e + 
-               sel$`Denisovan population growth rate`*f + g, 
-             data = sel, start = c(a=0,b=0,c=0,d=0,e=0,f=0,g=0))
-  #nonlinear square model-still not a great model. variables seem unconnected
-
 corel = rcorr(as.matrix(vals), type = "spearman")
-  #test for correlation
-
-p5 = ggplot(vals1, aes(x=vals1))+
-  geom_histogram(aes(y=..density..), color= "black", fill = "aquamarine",
-                 binwidth = 30)+
-  geom_density(alpha=.3, fill = "brown")+
-  #geom_vline(aes(yintercept = prior))+ #adds a unif dist to hist?   
-  xlab('Ancestral Population Size')+
-  ylab("Density")+
-  ggtitle("Selected Simulations")+
-  theme_minimal()
-p4
-    #this makes a nice hist of the selected sims for AncSize.     
+  #test for correlation     
 
 test = wilcox.test(vals1, param1)
 test$p.value
   #this tests whether the two samps are signif diff
   #they are - is because of huge diff in samp size?
 
-vals1[nrow(vals1)+9500,] = NA
-
-colnames(df3) = colnames(vals1)
-anc = merge(param1, vals1)
-
-#plotting total sims against accepted ####
-param2 = as.list(param2)
-vals1 = as.list(vals1)
-  #this solution only works when its as a list
-p = do.call(cbind, param2) 
-p.melt = melt(p) #makes the list into a df for abc_sim
-p.melt = p.melt %>% 
-  mutate(Var1 = 1:length(Var1))
-  #adds a length vector with the row number (makes it easier to plot)
-
-v = do.call(cbind, vals1)
-v.melt = melt(v) #makes the list into a df for rej$unadj.values
-v.melt = v.melt %>% 
-  mutate(Var1 = 1:length(Var1))
-  #adds a length vector with the row number (makes it easier to plot)
-
-ggplot()+
-  geom_line(data = v.melt, aes(y=v.melt$Var1, x=v.melt$value), 
-            color = "red")+
-  geom_point(data = p.melt, aes(y=p.melt$Var1, x=p.melt$value))+
-  theme_minimal()
-  #plots the two df on the same plot, smaller df as the line
-
-#posterior and prior hist plots (lansing et al 2017) ####
-par(mfrow=c(2,3))
-
-hist(param1,
-     main = "Ancestral Size",
-     border = "black",
-     col = "red")
-hist(param2,
-     main = "Mig rates P1",
-     border = "black",
-     col = "red")
-hist(param3,
-     main = "Mig rates P2",
-     border = "black",
-     col = "red",)
-  #hists above are all sims (prior)
-hist(vals1,
-     main = "Ancestral Size",
-     border = "black",
-     col = "darkorchid1",
-     breaks = 6)
-hist(vals2,
-     main = "Mig rates P1",
-     border = "black",
-     col = "darkorchid1",
-     breaks = 6)
-hist(vals3,
-     main = "Mig rates P2",
-     border = "black",
-     col = "darkorchid1",
-     breaks = 6)
-  #above are the selected sims (prior)
-#fitting ####
-gf = gfit(target = sum_stat_obs, 
-          sumstat = abc_sim$stats, 
-          nb.replicate = 10,
-          tol = 0.05
-          )
-gf
-  #this fits it - unsure how to interpret the results. 
-
-#d-stats ####
-mean(vals$V1)
-cohensD(vals$V1, mu = 248.5389)
-
-#abc_mcmc sims ####
-
-abc_sim2 = ABC_mcmc(
-  model = binary_model('/home/guy/splatche3/template/run_sim.sh'),
-  prior = prior,
-  summary_stat_target = sum_stat_obs,
-  n_rec = 100,
-  method = "Marjoram_original"
-)
-
-rej2 = abc(target = sum_stat_obs, 
-          abc_sim2$param, 
-          abc_sim2$stats, 
-          tol = 0.09, #could do rej right now
-          method = "rejection")
-
-#joint site freq spectrum ####
   
